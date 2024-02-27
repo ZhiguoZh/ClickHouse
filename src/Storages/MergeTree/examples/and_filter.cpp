ab39e55deded078e76014dc7c6faadc1cd1ad69f
Zhiguo Zhou <zhiguo.zhou@intel.com>
2024-02-27 17:13:30 +0800

    add microbench

---
+++ src/Storages/MergeTree/examples/and_filter.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <Columns/ColumnVector.h>
+#include <Common/Stopwatch.h>
+#include <Storages/MergeTree/MergeTreeRangeReader.cpp> // NOLINT
+
+using namespace DB;
+
+void measureAndFilters(size_t size)
+{
+    auto generateFastIncrementColumn = [](size_t len)->ColumnPtr
+    {
+        auto filter = ColumnUInt8::create(len);
+        auto & filter_data = filter->getData();
+
+        for (size_t i = 0; i < len; ++i)
+            filter_data[i] = static_cast<UInt8>(i & 0xFF);
+
+        return filter;
+    };
+
+    auto generateSlowIncrementColumn = [](size_t len)->ColumnPtr
+    {
+        auto filter = ColumnUInt8::create(len);
+        auto & filter_data = filter->getData();
+
+        for (size_t i = 0; i < len; ++i)
+            filter_data[i] = static_cast<UInt8>((i >> 8) & 0xFF);
+
+        return filter;
+    };
+
+    auto first_filter = generateFastIncrementColumn(size);
+    auto second_filter = generateSlowIncrementColumn(size);
+
+    {
+        Stopwatch watch;
+        auto result = andFilters(first_filter, second_filter);
+        std::cerr << watch.elapsedSeconds() << std::endl;
+    }
+}
+
+int main()
+{
+    size_t size = 10000000;
+    measureAndFilters(size);
+}
